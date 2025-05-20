64b5172c3b00b87ef9ac2afc947529dc62e02ae9
anshuu23 <anshumj321@gmail.com>
2025-05-20 21:01:30 +0530

    feat : added database schema

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import express from "express"
 import { getEnvVariables } from "./getEnv"
+const PORT = getEnvVariables().PORT
+
 
 const app = express()
 
-const PORT = getEnvVariables().PORT
+app.use(cors())
+app.use(express.json())
+app.use('/' , router)
+
 app.listen(PORT , ()=>{
     console.log(`server is listening on port ${PORT}`)
 })
+
+function cors(): any {
+    throw new Error("Function not implemented.")
+}
